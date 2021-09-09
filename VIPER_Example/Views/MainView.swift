f95774ac3449f3e43a644e90bae38405b60a7588
Chad Hamdan <chad.hamdan4@gmail.com>
2021-09-09 14:56:38 -0400

    Converted MainView to XIB

--- VIPER_Example/Views/MainView.swift
+++ VIPER_Example/Views/MainView.swift
@@ -1,29 +1,28 @@
 //
-//  mainView.swift
+//  MainView.swift
 //  VIPER_Example
 //
 //  Created by Chad Hamdan on 9/9/21.
 //
 
-import Foundation
 import UIKit
 
 class MainView: UIViewController, ViewProtocol {
     var presenter: PresenterProtocol?
     
-    private let label: UILabel = {
-        let label = UILabel(frame: CGRect(x: 10, y: 50, width: 230, height: 21))
-        label.text = "Hello World!"
-        return label
-    }()
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        view.backgroundColor = .white
-        
-        view.addSubview(label)
-        label.center = view.center
-        label.textAlignment = .center
     }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
 }
